dbe3b2cc3066f1d64af258b64ac69049ca6356e7
M3 Beatz <m3beatz@M3.local>
2024-08-08 18:25:42 -0400

    contract frontend

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom/client";
+import App from './App';
+import './index.css';
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+
+const manifestUrl = "";
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>,
+);
