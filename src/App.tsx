582b9e8c191293be0e5e0551a3850fff71604529
M3 Beatz <m3beatz@M3.local>
2024-08-09 03:06:32 -0400

    added mixer bridge

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { TonConnectButton } from '@tonconnect/ui-react';
 import { useMainContract } from './hooks/useMainContract';
 import { useTonConnect } from './hooks/useTonConnect';
 import { fromNano } from 'ton-core';
+import { useState } from 'react';
 
 function App() {
   const {
@@ -16,51 +17,60 @@ function App() {
 
   const { connected } = useTonConnect();
 
+  const [currentView, setCurrentView] = useState(0);
+
   return (
     <div>
       <div>
-        <TonConnectButton />
+        <button style={{ position: 'absolute', top: 0, left: 0, width: '50vw', height: '8vh', border: currentView === 0 ? '1px solid #66bb6a' : '', color: currentView === 0 ? 'green' : 'white'}} onClick={() => setCurrentView(0)}>$MIXER Bridge</button>
+        <button disabled={true} style={{ position: 'absolute', top: 0, right: 0, width: '50vw', height: '8vh', border: currentView === 1 ? '1px solid #66bb6a' : '', color: currentView === 1 ? 'green' : 'white'}}>Staking (Soon)</button>
       </div>
-      <div>
-        <div className='Card'>
-          <b>Our contract address</b>
-          <div className='Hint'>{contract_address?.slice(0,30) + '...'}</div>
-          <b>Our contract balance</b>
-          {contract_balance && (<div className='Hint'>{fromNano(contract_balance)}</div>)}
+      <iframe style={{ display: currentView == 0 ? 'flex' : 'none', border: "0", margin: 0, padding: 0, width: "100vw", height: "92vh", position: "absolute", top: '8vh', left: 0 }} src='https://marcuscodewriter.github.io/fluttermix'></iframe>
+      <div style={{ display: currentView == 1 ? 'block' : 'none' }}>
+        <div>
+          <TonConnectButton />
         </div>
+        <div>
+          <div className='Card'>
+            <b>Our contract address</b>
+            <div className='Hint'>{contract_address?.slice(0,30) + '...'}</div>
+            <b>Our contract balance</b>
+            {contract_balance && (<div className='Hint'>{fromNano(contract_balance)}</div>)}
+          </div>
 
-        <div className='Card'>
-          <b>Counter value</b>
-          <div>{String(counter_value) ?? "Loading..."}</div>
-        </div>
+          <div className='Card'>
+            <b>Counter value</b>
+            <div>{String(counter_value) ?? "Loading..."}</div>
+          </div>
 
-        {connected && (
-          <a onClick={() => {
-            sendIncrement();
-          }}>
-            Increment by 5
-          </a>
-        )}
+          {connected && (
+            <a onClick={() => {
+              sendIncrement();
+            }}>
+              Increment by 5
+            </a>
+          )}
 
-        <br/>
+          <br/>
 
-        {connected && (
-          <a onClick={() => {
-            sendDeposit();
-          }}>
-            Request deposit of 1 TON
-          </a>
-        )}
+          {connected && (
+            <a onClick={() => {
+              sendDeposit();
+            }}>
+              Request deposit of 1 TON
+            </a>
+          )}
 
-        <br/>
+          <br/>
 
-        {connected && (
-          <a onClick={() => {
-            sendWithdrawalRequest();
-          }}>
-            Request 0.7 TON withdrawal
-          </a>
-        )}
+          {connected && (
+            <a onClick={() => {
+              sendWithdrawalRequest();
+            }}>
+              Request 0.7 TON withdrawal
+            </a>
+          )}
+          </div>
       </div>
     </div>
   );
