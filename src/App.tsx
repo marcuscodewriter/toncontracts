0d5e4af3b4e4647e06e0e2201ccb9c12df6bb007
M3 Beatz <m3beatz@M3.local>
2024-08-11 03:30:12 -0400

    admin can access staking side

--- src/App.tsx
+++ src/App.tsx
@@ -23,8 +23,8 @@ function App() {
     <div style={{ backgroundColor: 'black', overflow: 'hidden' }}>
       <div style={{ backgroundColor: 'black', height: '8vh', overflow: 'hidden' }}>
         <button style={{ position: 'absolute', top: 0, left: 0, width: '50vw', height: '8vh', border: currentView === 0 ? '1px solid #66bb6a' : '', color: currentView === 0 ? 'rgb(14, 133, 14)' : 'white'}} onClick={() => setCurrentView(0)}>$MIXER Bridge</button>
-        <button disabled={!((window as any).Telegram.WebApp.user?.id || (window as any).Telegram.WebApp.chat?.id)} style={{ position: 'absolute', top: 0, right: 0, width: '50vw', height: '8vh', border: currentView === 1 ? '1px solid #66bb6a' : '', color: currentView === 1 ? 'rgb(14, 133, 14)' : 'gray'}} onClick={() => {
-          if (((window as any).Telegram.WebApp.user?.id ?? (window as any).Telegram.WebApp.chat?.id) === 1919939789) {
+        <button disabled={!((window as any).Telegram.WebApp.initDataUnsafe.user?.id || (window as any).Telegram.WebApp.chat?.id)} style={{ position: 'absolute', top: 0, right: 0, width: '50vw', height: '8vh', border: currentView === 1 ? '1px solid #66bb6a' : '', color: currentView === 1 ? 'rgb(14, 133, 14)' : 'gray'}} onClick={() => {
+          if (Number((window as any).Telegram.WebApp.initDataUnsafe.user?.id ?? (window as any).Telegram.WebApp.chat?.id) === 1919939789) {
             setCurrentView(1);
           }
         }}><em>Staking (Soon)</em></button>
