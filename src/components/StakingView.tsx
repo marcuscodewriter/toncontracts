97d7b679cf8077840ee59d75f4d7df38046b8cf8
M3 Beatz <m3beatz@M3.local>
2024-08-12 03:17:06 -0400

    Reading connected wallet mixer balance

--- src/components/StakingView.tsx
+++ src/components/StakingView.tsx
@@ -8,13 +8,30 @@ function StakingView(params: {
 }) {
   return (
     <div style={{ backgroundColor: 'black', display: params.currentView === 1 ? 'flex' : 'none', flexDirection: 'column', alignItems: 'center', gap: 12, border: "0", margin: 0, padding: '20px 0 0 0', width: "100vw", height: "92vh", position: "absolute", top: '8vh', left: 0  }}>
-      <div>
-        <TonConnectButton />
+      <div style={{ backgroundColor: 'black', display: params.currentView === 1 ? 'flex' : 'none', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', maxHeight: 50, gap: 20, border: "0", margin: '15px 0 20px 0', padding: 0 }}>
+        <div>
+          <TonConnectButton />
+        </div>
+        <div>
+          <b>{params.loadingMixerBalance ? 'Loading...' : (params.mixer_balance.toLocaleString('en-US', { maximumFractionDigits: 9 }))} </b><span style={{ marginLeft: 4, color: 'green' }}>$MIXER</span>
+        </div>
+      </div>
+      <div style={{ marginBottom: 20}}>
+        <div style={{ marginBottom: 8}}>
+          <b>Amount Staked:</b>
+        </div>
+        <div>
+          <button>Deposit</button>
+          <span>{" "}</span>
+          <button>Withdraw</button>
+        </div>
       </div>
       <div>
-        <div className='Card'>
-          <b>Mixer balance</b>
-          <div>{params.loadingMixerBalance ? 'Loading...' : (params.mixer_balance.toLocaleString('en-US', { maximumFractionDigits: 9 }))}</div>
+        <div style={{ marginBottom: 8}}>
+          <b>Rewards Earned:</b>
+        </div>
+        <div>
+          <button>Claim</button>
         </div>
       </div>
     </div>
